82d94090a1fd67705d8f078700b662208b22d6ec
Cameron Lino <cameronlino@outlook.com>
2026-09-20 18:34:14 -0300

    Hola Mundo:  complete lesson 1

---
+++ src/main/java/com/smarttask/Main.java
@@ -0,0 +1,19 @@
+package com.smarttask;
+
+/**
+ * Punto de entrada de la aplicación SmartTask.
+ *
+ * <p>Esta clase contiene el método principal que inicia
+ * la ejecución del programa.</p>
+ */
+public class Main {
+
+    /**
+     * Inicia la ejecución de SmartTask.
+     *
+     * @param args argumentos recibidos desde la línea de comandos
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
